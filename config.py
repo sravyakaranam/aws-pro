7aa212748c0ab287bd664f755e87c3141fc1cb3a
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2025-02-10 23:44:03 -0500

    code

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.crwo6ima66eh.us-east-2.rds.amazonaws.com"
+customuser = "cloudpro"
+custompass = "Cloudpro2025"
+customdb = "employee"
+custombucket = "addiemployee"
+customregion = "us-east-2"  # Remove the 'c' (use 'us-east-2' instead of 'us-east-2c')
